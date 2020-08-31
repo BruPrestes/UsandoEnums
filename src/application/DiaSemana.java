567d5cc10876b99a5bc828a20948dba53f493049
Bruno <brunnoprestes@gmail.com>
2020-08-30 22:42:57 -0300

    Usando Enums

---
+++ src/application/DiaSemana.java
@@ -0,0 +1,29 @@
+package application;
+
+/**
+ *
+ * @author Bruno Prestes
+ */
+public enum DiaSemana {
+    SEGUNDA(1),
+    TERCA(2),
+    QUARTA(3),
+    QUINTA(4),
+    SEXTA(5),
+    SABADO(6),
+    DOMINGO(7);
+    
+    private int valor;
+
+    DiaSemana(int valor) {
+        this.valor = valor;
+    }
+
+    
+    public int getValor() {
+        return valor;
+    }
+    
+    
+    
+}
